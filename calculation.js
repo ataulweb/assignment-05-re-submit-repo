08138e313c1c86a820a3b07ef09189dfcba407b7
ataulweb <ataulweb@gmail.com>
2022-08-31 17:40:22 +0600

    calculation java script text added

--- calculation.js
+++ calculation.js
@@ -1,11 +1,11 @@
-// Calculate Button On Trigger
+
 document.getElementById('btn-calculate').addEventListener('click', function(){
     const playerCost = getValueFromField('perplayer-field');
     const playerTotalCost = playerCost * 5;
     const cost = setValueToElement('player-total', playerTotalCost);
 })
 
-// Calculate All Button On Trigger
+
 document.getElementById('btn-total').addEventListener('click', function(){
     const updatePlayerCost = getValueFromElement('player-total');
     const managerCost = getValueFromField('manager-field');
@@ -14,7 +14,7 @@ document.getElementById('btn-total').addEventListener('click', function(){
     const total = setValueToElement('total-cost', totalSum);
 })
 
-// Function For Getting Values From Input field
+
 function getValueFromField(fieldId){
     const inputField = document.getElementById(fieldId);
     const inputFieldString = inputField.value;
@@ -22,7 +22,7 @@ function getValueFromField(fieldId){
     return inputFieldValue;
 }
 
-// Function For Getting Value From Element
+
 function getValueFromElement(elementId){
     const elementPreviousValue = document.getElementById(elementId);
     const elementPreviousValueString = elementPreviousValue.innerText;
@@ -30,7 +30,7 @@ function getValueFromElement(elementId){
     return newElementValue;
 }
 
-// Function For Setting Value To Element 
+
 function setValueToElement(setElementId, value){
     const elementText = document.getElementById(setElementId);
     const elementTextString = elementText.innerText;
